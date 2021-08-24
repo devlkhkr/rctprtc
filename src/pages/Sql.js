a5ff31659cf7408142000e28e110ef2735694454
leeakang <56522981+leeakang@users.noreply.github.com>
2021-08-24 19:04:28 +0900

    SQL 데이터 불러오기
    SQL 데이터 불러오기

--- src/pages/Sql.js
+++ src/pages/Sql.js
@@ -1,34 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import axios from 'axios'
 
-const Sql = () => {
-    return (
-        <div>
-            <div className="wrapper_tool">
-                <h3>MySQL Test</h3>
-                <button onClick={()=>{ return onDataHandler() }}>GetData</button>
-            </div>
-        </div>
-    );
-    function onDataHandler(){
-        try{
-            const axiosSet = axios.create({
-                baseURL: "http://localhost:3001/",
-                Headers:{
-                    'Content-type': 'application/json; charset=UTF-8'
-                }, 
-                responseType: 'json',
-                responeseEncoding: 'json'
-            })
-            const res=axiosSet.get('/chats')
-            res.then(function(res){
-                console.log(res.data)
-            })
-        }
-        catch(err){
-            console.log(err)
-        }
+class Sql extends Component {
+    constructor(props)
+    {
+        super(props);
+        this.getData = this.getData.bind(this)
+        this.state = {
+            chats: "before",
+        };
     }
+    state = {
+        test: "Test"
+    }
+    getData = () => {
+        const axiosSet = axios.create({
+            baseURL: "http://localhost:3001/",
+            Headers:{
+                'Content-type': 'application/json; charset=UTF-8'
+            }, 
+            responseType: 'json',
+            responeseEncoding: 'json'
+        })
+        const res=axiosSet.get('/chats')
+        res.then(function(res){
+            console.log(res.data)
+        })
+        this.setState({
+            chats: "after"
+        })
+    }    
+    render(){
+        return (
+            <div>
+                <div className="wrapper_tool">
+                    <h3>MySQL Test</h3>
+                    <button onClick={ this.getData }>GetData</button>
+                </div>
+                <div>{this.state.chats}</div>
+            </div>
+        );
+    }
+
 };
 
 export default Sql;

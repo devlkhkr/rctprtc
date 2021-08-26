6b3617d81fe3f9588b73704e108d2dec325e7bca
leeakang <56522981+leeakang@users.noreply.github.com>
2021-08-26 17:59:34 +0900

    화면에 출력하는 여러가지 방법 연구
    화면에 출력하는 여러가지 방법 연구

--- src/pages/Sql.js
+++ src/pages/Sql.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ReactDOM from "react-dom";
 import axios from 'axios'
 
 class Sql extends Component {
@@ -7,7 +8,9 @@ class Sql extends Component {
         super(props);
         this.getData = this.getData.bind(this);
         this.state = {
-            test: "before",
+            id: "id",
+            name: "name",
+            phone: "phone",
         };
     }
     getData = async() => {
@@ -21,27 +24,55 @@ class Sql extends Component {
         })
         const res = await axiosSet.get('/chats')
 
-        for(var i=0;i<res.data.length;i++){
-            console.log([res.data[i]._id, res.data[i].name, res.data[i].phone]) 
-
-            const props = {className: "test", onClick: () => {}};
-            const child = <span>{res.data[i]}</span>
-            React.createElement('div', props, child)
-
-        }
         this.setState({
-            test: "after"
+            id: res.data[0]._id,
+            name: res.data[0].name,
+            phone: res.data[0].phone
         })
+        
+        function test(){
+            var addHTML = "";
+            for(var i=0;i<res.data.length;i++){
+                addHTML += res.data[i]._id + res.data[i].name + res.data[i].phone
+            }
+            return addHTML
+        }
+
+        ReactDOM.render(test(), document.getElementById('data'))
+
     }
 
     render(){
+
+        const menus = ["Menu1", "Menu2", "Menu3", "Menu4"]
+        const menuList = menus.map((menu) => (<li>{menu}</li>));
+
         return (
             <div>
                 <div className="wrapper_tool">
                     <h3>SQL Test</h3>
                     <button onClick={ this.getData }>GetData</button>
                 </div>
-                <div>{this.state.test}</div>
+
+                <div>
+                    {this.state.id}
+                    {this.state.name}
+                    {this.state.phone}
+                </div>
+
+                <div id="data"></div>
+
+                {/* s : render 안에서 생성 */}
+                <ul>{menuList}</ul>
+                {/* s : render 안에서 생성 */}
+
+                <ul>
+                    {["Item1", "Item2", "Item3"].map(item =>
+                        <li key="{item}">{item}</li>
+                    )}
+                </ul>
+                {/* e : return 안에서 생성 */}
+
             </div>
         );
     }

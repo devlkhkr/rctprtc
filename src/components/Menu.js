4fff50057e7e24d87bd26f0616367c4e2ef65312
leeakang <56522981+leeakang@users.noreply.github.com>
2021-09-06 14:55:36 +0900

    menu.js innerHTML -> title

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -5,20 +5,20 @@ class Menu extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            currentPage: "Main"
+            currentPage: "Home"
         }
         this.currentChange = this.currentChange.bind(this)
     }
     currentChange = (e) => {
         this.setState({
-            currentPage: e.target.innerHTML,
+            currentPage: e.target.getAttribute("title"),
         })
     }
     render(){
         return (
         <div>
             <div className="header">
-                <h1><NavLink exact to="/" activeClassName="on">TestWeb</NavLink></h1>
+                <h1><NavLink exact to="/" activeClassName="on" onClick={this.currentChange} title="Home">Home</NavLink></h1>
                 <div className="menus">
                     <input type="checkbox" id="navTrigger"></input>
                     <label htmlFor="navTrigger" className="ham">
@@ -26,13 +26,12 @@ class Menu extends Component {
                     </label>
                     <label htmlFor="navTrigger" className="mask"></label>
                     <ul className="nav">
-                        <li><NavLink exact to="/" activeClassName="on" onClick={this.currentChange}>Main</NavLink></li>
-                        <li><NavLink exact to="/about" activeClassName="on" onClick={this.currentChange}>About</NavLink></li>
-                        <li><NavLink to="/posts" activeClassName="on" onClick={this.currentChange}>Post</NavLink></li>
-                        <li><NavLink to="/tools" activeClassName="on" onClick={this.currentChange}>Tools</NavLink></li>
-                        <li><NavLink to="/sql" activeClassName="on" onClick={this.currentChange}>SQLTest</NavLink></li>
-                        <li><NavLink to="/bind" activeClassName="on" onClick={this.currentChange}>BindTest</NavLink></li>
-                        <li><NavLink to="/class" activeClassName="on" onClick={this.currentChange}>ClassTest</NavLink></li>
+                        <li><NavLink exact to="/about" activeClassName="on" onClick={this.currentChange} title="About">About</NavLink></li>
+                        <li><NavLink to="/posts" activeClassName="on" onClick={this.currentChange} title="Post">Post</NavLink></li>
+                        <li><NavLink to="/tools" activeClassName="on" onClick={this.currentChange} title="Tools">Tools</NavLink></li>
+                        <li><NavLink to="/sql" activeClassName="on" onClick={this.currentChange} title="SQLTest">SQLTest</NavLink></li>
+                        <li><NavLink to="/bind" activeClassName="on" onClick={this.currentChange} title="BindTest">BindTest</NavLink></li>
+                        <li><NavLink to="/class" activeClassName="on" onClick={this.currentChange} title="ClassTest">ClassTest</NavLink></li>
                     </ul>
                 </div>
             </div>
